b4b155159623a531d38dab55d969e7a2ddf8f969
10920 <jun.guo@hand-china.com>
2016-08-02 21:28:57 +0800

    Exam of net and socket

--- Exam1/src/main/java/com/hand/App.java
+++ Exam1/src/main/java/com/hand/App.java
@@ -1,49 +1,40 @@
 package com.hand;
 
+
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-/*
- *  (8 分)	使用程序从网上下载pdf,网址为 http://files.saas.hand-china.com/java/target.pdf,
- *  保存在本地, 编程时使用带缓冲的读写,将需要保证保存后的pdf文件能正常 
- */
+
 public class App {
 	public static void main(String[] args) {
-		new ReadByGet().start();
-	}
-	static class ReadByGet extends Thread{
-		public void run(){
-			try {
-				
-				URL url = new URL("http://files.saas.hand-china.com/java/target.pdf");
-				URLConnection connection = url.openConnection();//建立url连接
-				InputStream is = connection.getInputStream();
-				BufferedInputStream bi = new BufferedInputStream(is,5000);
-				FileOutputStream fos = new FileOutputStream(new File("target.pdf"));
-				BufferedOutputStream bos = new BufferedOutputStream(fos,5000);
-				byte b[] = new byte[1000];
-				while(bi.read(b)!=-1){
-					bos.write(b);
-				}
-				bos.flush();
-				bos.close();
-				fos.close();
-				bi.close();
-				is.close();
-			
-			} catch (MalformedURLException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		URL url;
+		try {
+			url = new URL("http://files.saas.hand-china.com/java/target.pdf");
+			URLConnection urlConnection = url.openConnection();
+			InputStream inputStream = urlConnection.getInputStream();
+			BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+
+			byte[] b = new byte[1024];
+			FileOutputStream fileOutputStream = new FileOutputStream("target.pdf", true);
+			int count = bufferedInputStream.read(b, 0, 1023);
+			while (count > 0) {
+				fileOutputStream.write(b, 0, count);
+				count = bufferedInputStream.read(b, 0, 1023);
 			}
+
+			fileOutputStream.close();
+			bufferedInputStream.close();
+			inputStream.close();
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+
 	}
 }
